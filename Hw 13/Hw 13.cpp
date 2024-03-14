c03a40f03007a117b5d6f1edc17200127364254f
momen <momen@MILF_HUNTER>
2024-03-14 13:29:18 +0200

    Добавьте файлы проекта.

---
+++ Hw 13/Hw 13.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+
+void power_f(int num1, int num2)
+{
+    int power = 1;
+    for (int i = num2; i > 0; i--) {                           // Обычная функция
+        power *= num1;
+    }
+    cout << "Simple func = " << power << "\n";
+}
+
+void power_p(int num1, int num2, int *power)
+{
+    *power = 1;
+    for (int i = num2; i > 0; i--) {                           // На указателе
+        *power *= num1;
+    }
+}
+int main()
+{
+    cout << "Input num1: ";
+    int num1;
+    cin >> num1;
+    cout << "Input num2: ";
+    int num2;
+    cin >> num2;
+    int result = 0;
+    power_f(num1, num2);
+    power_p(num1, num2, &result);
+    cout << "Pointer func = " << result;
+}
+
+
